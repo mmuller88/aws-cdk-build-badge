926e904d51ecc926fd5b5f1790ad30a5b1d7a256
martin.mueller <m.mueller@unimed.de>
2021-01-10 14:17:47 +0100

    feat: get rid of the direct cdk dependency

--- .projenrc.js
+++ .projenrc.js
@@ -1,30 +1,35 @@
 const { AwsCdkConstructLibrary } = require('projen');
 
+const cdkVersion = '1.83.0';
+
 const deps = [
   '@types/aws-lambda',
   'aws-lambda',
   'aws-sdk',
   'esbuild@^0',
+  `@aws-cdk/assert@${cdkVersion}`,
+];
+
+const cdkDeps = [
+  `@aws-cdk/aws-apigateway@${cdkVersion}`,
+  `@aws-cdk/aws-lambda@${cdkVersion}`,
+  `@aws-cdk/core@${cdkVersion}`,
+  `@aws-cdk/aws-iam@${cdkVersion}`,
+  `@aws-cdk/aws-lambda-nodejs@${cdkVersion}`,
 ];
 
 const project = new AwsCdkConstructLibrary({
   authorAddress: 'damadden88@googlemail.de',
   authorName: 'martin.mueller',
-  cdkVersion: '1.83.0',
-  cdkVersionPinning: true,
+  // cdkVersion: '1.83.0',
+  // cdkVersionPinning: true,
   name: 'aws-cdk-build-badge',
   repositoryUrl: 'https://github.com/mmuller88/aws-cdk-build-badge',
   codeCov: true,
   projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
-  cdkDependencies: [
-    '@aws-cdk/aws-apigateway',
-    '@aws-cdk/aws-lambda',
-    '@aws-cdk/core',
-    '@aws-cdk/aws-iam',
-    '@aws-cdk/aws-lambda-nodejs',
-  ],
-  devDeps: deps,
+  devDeps: [...deps, ...cdkDeps],
   deps: deps,
+  peerDeps: cdkDeps,
   bundledDeps: deps,
   keywords: [
     'cdk',
